b73e7cca77ce4fec6ab311cd09609963ce8a5eb0
Hemant Sharma <hemants216@gmail.com>
2023-03-16 17:21:20 +0530

    added-backdrop

--- src/components/SingleContent.jsx
+++ src/components/SingleContent.jsx
@@ -3,7 +3,7 @@ import AppContext from "../store/context";
 import { useParams } from "react-router-dom";
 const SingleContent = () => {
   const { media_type, id } = useParams();
-  const { fetchSingle, single ,darkMode} = useContext(AppContext);
+  const { fetchSingle, single, darkMode } = useContext(AppContext);
   useEffect(() => {
     fetchSingle(media_type, id);
   }, []);
@@ -12,36 +12,70 @@ const SingleContent = () => {
   const voteAverage =
     ((single.vote_average / 10) * 100).toFixed().toString() + "%";
   return (
-    <div className="my-7 mx-auto grid grid-cols-1 xl:grid-cols-3 w-[90%] ">
-      <div className="h-[500px] xl:h-[600px] w-80 xl:w-96 overflow-hidden mx-auto xl:mx-0 ">
-        <img src={poster} alt={"image-loading"} />
-      </div>
-      <div className="flex flex-col gap-4 col-span-2">
-        <h3 className={`${darkMode?'text-whitish':'text-blackish'} text-5xl font-light`}>
-          {single.name ? single.name : single.original_title}
-        </h3>
-        {single.genres ? (
-          <div className="genres flex gap-3">
-            <span className={`${darkMode?'bg-whitish text-blackish':'bg-blackish text-whitish'} py-1 px-2`}>
-              {single.genres[0].name}
-            </span>
-            {single.genres[1] ? (
-               <span className={`${darkMode?'bg-whitish text-blackish':'bg-blackish text-whitish'} py-1 px-2`}>
-                {single.genres[1].name}
-              </span>
-            ) : null}
-          </div>
-        ) : null}
-
-        <div className={`${darkMode?'text-whitish':'text-blackish'}`}>{single.overview}</div>
-
-        <div class="w-full  rounded-full bg-gray-700">
-          <div
-            className={`bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full `}
-            style={{ width: `${voteAverage}` }}
+    <div
+      className={`min-h-screen w-full relative after:absolute after:h-full after:w-full  ${
+        darkMode
+          ? "after:bg-[rgba(0,0,0,0.5)]"
+          : "after:bg-[rgba(255,255,255,0.42)]"
+      }  after:top-0 after:left-0 after:z-[-1]`}
+      style={{
+        background: `url(${backdrop})`,
+        backgroundPosition: "center center",
+        objectFit: "cover",
+        zIndex: "-2",
+      }}
+    >
+      <div className="py-7 mx-auto grid grid-cols-1 xl:grid-cols-3 w-[90%] justify-items-center z-20">
+        <img
+          src={poster}
+          alt={"image-loading"}
+          className="h-[500px] xl:h-[600px] w-80 xl:w-96 object-cover"
+        />
+        <div className="flex flex-col gap-4 col-span-2">
+          <h3
+            className={`${
+              darkMode ? "text-whitish" : "text-blackish"
+            } text-5xl font-light`}
           >
-            {""}
-            {voteAverage}
+            {single.name ? single.name : single.original_title}
+          </h3>
+          {single.genres ? (
+            <div className="genres flex gap-3">
+              <span
+                className={`${
+                  darkMode
+                    ? "bg-whitish text-blackish"
+                    : "bg-blackish text-whitish"
+                } py-1 px-2`}
+              >
+                {single.genres[0].name}
+              </span>
+              {single.genres[1] ? (
+                <span
+                  className={`${
+                    darkMode
+                      ? "bg-whitish text-blackish"
+                      : "bg-blackish text-whitish"
+                  } py-1 px-2`}
+                >
+                  {single.genres[1].name}
+                </span>
+              ) : null}
+            </div>
+          ) : null}
+
+          <div className={`${darkMode ? "text-whitish" : "text-blackish"}`}>
+            {single.overview}
+          </div>
+
+          <div class="w-full  rounded-full bg-gray-700">
+            <div
+              className={`bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full `}
+              style={{ width: `${voteAverage}` }}
+            >
+              {""}
+              {voteAverage}
+            </div>
           </div>
         </div>
       </div>
